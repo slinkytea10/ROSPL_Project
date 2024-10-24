d7e376b4a3bc4f7c15fc9d80be585a2f2613b1bc
slinkytea10 <124913065+slinkytea10@users.noreply.github.com>
2024-10-24 15:35:02 +0530

    Add files via upload

--- Home/urls.py
+++ Home/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import HomeView, get_teachers
+from .views import AboutView,ContactView,HomeView, get_teachers
 
 urlpatterns = [
     path('', HomeView.as_view(), name='HomeView'),
+    path('about/', AboutView.as_view(template_name='Home/about.html'), name='AboutView'),
+    path('contact/', ContactView.as_view(template_name='Home/contact.html'), name='ContactView'),
+    #path('login/',
+         #auth_views.LoginView.as_view(template_name='Review/login.html'),
+         #name='LoginView'),
+
     path('ajax_calls/autocomplete_search_func', get_teachers, name='get_teachers'),
 ]
